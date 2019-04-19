862c36247e4c9c69bb0a75f9ff26c9ec9745884c
Prabu Jayapandian <Prabu.Jayapandian@dmh.ms.gov>
2019-04-19 16:16:54 -0500

    Test

--- src/main/java/com/jp/preauth/controller/TestContoller.java
+++ src/main/java/com/jp/preauth/controller/TestContoller.java
@@ -2,7 +2,9 @@ package com.jp.preauth.controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,11 +19,20 @@ public class TestContoller {
 	
 	
 	@RequestMapping("/sayHello")
-	public String sayHello(Principal princi,  @RequestHeader HttpHeaders headers, HttpServletRequest request){
+	public String sayHello(Principal princi,  @RequestHeader HttpHeaders headers, HttpServletRequest request, HttpServletResponse response){
+		
+		System.out.println(request.getSession().getAttribute("UserDetails"));
 		
 		UserDetails userDetails =
 				 (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		System.out.println(userDetails);
+		
+		
+		//response.addCookie(new Cookie("foo", "bar"));
+
+		
+		request.getSession().setAttribute("UserDetails", userDetails);
+		
 		return "Hello World from preauth";
 	}
 	
